761e2244ee189ae163e8b0d6ed1a60bc98e27270
Ashwin P Chandran <ashchandran@deloitte.com>
2017-04-26 01:58:25 +0530

    integrating graphs

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,3 +1,44 @@
+var graph = new GraphBuilder();
+
+// Graph Builder
+function GraphBuilder() {
+    
+}
+
+GraphBuilder.prototype.init = function () {
+
+    // Display graph selector
+    var $graphSelect = this.dropdown("Graph", [
+        {value: "scatter", label: "Scatter Plot"}
+    ], "graph-form");
+
+    $graphSelect.on("click", function () {
+        
+    })
+}
+
+GraphBuilder.prototype.dropdown = function (label, options, container) {
+    var $options = [];
+
+    $options.push($("<option></option>").attr("disabled", true).attr("selected", true).text("Choose Option"))
+    options.forEach(function (option) {
+        $option = $("<option></option>").val(option.value).text(option.label);
+        $options.push($option);
+    });
+
+
+    var $dropdown = $("<li></li>").append(
+        $("<select></select>").append($options),
+        $("<label></label>").html(label)
+    );
+
+    $("#"+container).append($dropdown);
+    $dropdown.children().first().material_select();
+    return $dropdown.children().first();
+}
+
+
+// Document Init
 $(document).on("dash-init", function () {
     $graphs = $(".graphs");
     // $(".btn-floating").sidenav();
@@ -8,5 +49,23 @@ $(document).on("dash-init", function () {
 
     $("#add-graph").on("click", function () {
         $(this).removeClass("pulse");
+        $("#graph-builder").toggle();
+
+        graphTest({});
     });
+
+    graph.init();
 });
+
+function graphTest(opt) {
+    var graphEle = opt.element || "graph-area";
+    var data = opt.data || [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16],
+        mode: 'markers',
+        type: "scatter"
+    }];
+
+	Plotly.plot(graphEle, data);
+}
+
